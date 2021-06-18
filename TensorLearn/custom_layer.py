0ab874ef92b3ae04214e2f2d0dcb87d1e72a77cb
Vitaliy Zema <vitaliy.zema@gmail.com>
2021-06-18 13:09:33 +0300

    ignoring gitignore

---
+++ TensorLearn/custom_layer.py
@@ -0,0 +1,26 @@
+#%%
+# Prepare environment
+import sys
+import random
+import numpy as np
+#import tensorflow.experimental.numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.python.keras.utils.vis_utils import plot_model
+from PIL import Image, ImageFont, ImageDraw
+
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Input
+from tensorflow.keras.optimizers import Adam, RMSprop
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.utils import to_categorical
+
+# Import datasets
+from tensorflow.keras.datasets import fashion_mnist
+
+# Load utils and configure GPU
+sys.path.append("../")
+from ml_utils import *
+restrict_gpu_mem(7000)
+
+0
--- ml_utils.py
+++ ml_utils.py
@@ -41,3 +41,31 @@ def restrict_gpu_mem(memory=5000):
     except RuntimeError as e:
       # Virtual devices must be set before GPUs have been initialized
       print(e)
+
+
+"""
+#%%
+# Prepare environment
+import sys
+import random
+import numpy as np
+#import tensorflow.experimental.numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.python.keras.utils.vis_utils import plot_model
+from PIL import Image, ImageFont, ImageDraw
+
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Input
+from tensorflow.keras.optimizers import Adam, RMSprop
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.utils import to_categorical
+
+# Import datasets
+from tensorflow.keras.datasets import fashion_mnist
+
+# Load utils and configure GPU
+sys.path.append("../")
+from ml_utils import *
+restrict_gpu_mem(7000)
+"""
